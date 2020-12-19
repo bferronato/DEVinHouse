df4826f61c28e9398145d986bea5dc5031184d3c
Bruno Henrique Ferronato <bferronato@gmail.com>
2020-12-19 14:50:58 -0300

    Basic React project

---
+++ Projeto03/escola-amorinha/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Basic React Project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
