588e55cb3bac51eb1500d06b8b75bad7ea069ca3
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-01-24 19:40:24 -0300

    Exclusão arquivos desnecessários

--- Projeto02/busca-de-processos/src/components/H1.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function H1(props) {
-    return (
-        <h1>OKKKKKK</h1>
-    )
-}
-
-export default H1
--- Projeto02/busca-de-processos/src/index.js
+++ Projeto02/busca-de-processos/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import routes from './routes';
 
-// const ambiente = process.env.PUBLIC_URL;
-// console.log(routes)
-// console.log(JSON.stringify(routes))
-
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
     <Switch>
