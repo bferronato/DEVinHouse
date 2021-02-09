2031d93ef4ca934a4e116b4810c2b6106b794a7d
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-09 19:27:44 -0300

    Introdução a java

---
+++ Semana10/IntroducaoJava/src/basico/OlaMundo.java
@@ -0,0 +1,15 @@
+package basico;
+
+import java.util.Date;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Olá");
+		System.out.println("Tudo bem?");
+		
+		Date data = new Date();
+		System.out.println(data);
+
+	}
+}
