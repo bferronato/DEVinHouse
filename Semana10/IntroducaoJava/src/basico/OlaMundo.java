0d907f9adfc9c36f93b9976b50117bd269c53691
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-09 20:03:15 -0300

    Printf

--- Semana10/IntroducaoJava/src/basico/OlaMundo.java
+++ Semana10/IntroducaoJava/src/basico/OlaMundo.java
@@ -10,6 +10,11 @@ public class OlaMundo {
 		
 		Date data = new Date();
 		System.out.println(data);
+		
+		long teste = (long) 1147483648;
+		System.out.println(teste);
 
+		double altura = 1.72;
+		System.out.printf("Minha altrura é %.1f", altura);
 	}
 }
