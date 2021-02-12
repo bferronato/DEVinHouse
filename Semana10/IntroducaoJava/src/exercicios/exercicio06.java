542e729ba42a7382ca5cbfa91e99eaf65e0ac667
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-11 22:46:16 -0300

    Comments

--- Semana10/IntroducaoJava/src/exercicios/exercicio06.java
+++ Semana10/IntroducaoJava/src/exercicios/exercicio06.java
@@ -1,7 +1,5 @@
 package exercicios;
 
-import java.util.*;
-
 public class exercicio06 {
 
 	public static void main(String[] args) {
@@ -11,21 +9,21 @@ public class exercicio06 {
 
 		Tipos primitivos em Java: 
 			
-		(i) boolean;
+		(i) boolean
 		(c) conjunto de valores falso ou verdadeiro.
-		(C) V, F; 
+		(C) V, F
 		
-		(ii) caracter;
-		(b) qualquer conjunto de caracteres alfanuméricos; 
-		(A) “Fulano tem 25 anos de carreira”;
+		(ii) caracter
+		(b) qualquer conjunto de caracteres alfanuméricos
+		(A) “Fulano tem 25 anos de carreira”
 		
-		(iii) inteiro;
-		(a) qualquer número inteiro, seja ele negativo, nulo ou positivo;
+		(iii) inteiro
+		(a) qualquer número inteiro, seja ele negativo, nulo ou positivo
 		(D) -78.
 		
 		(iv) real.
 		(d) qualquer número real, seja ele negativo, nulo ou positivo.
-		(B) 35.8;
+		(B) 35.8
 		 
 		 */
 	}
