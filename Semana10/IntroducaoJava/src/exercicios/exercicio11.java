7f56a6a5d6da1ae044131ae2b8c2d2d14ac17c1e
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-16 23:53:44 -0300

    Exercício 11

--- Semana10/IntroducaoJava/src/exercicios/exercicio11.java
+++ Semana10/IntroducaoJava/src/exercicios/exercicio11.java
@@ -1,41 +1,35 @@
 package exercicios;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class exercicio11 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Scanner teclado = new Scanner(System.in);
-		
-		System.out.print("Qual a sua data de nascimento? ");
-		
-//		numeroDigitado = teclado.next
 
-//		int numeroDigitado;
-//		int aleatorio;
-//
-//		do {
-//
-//			numeroDigitado = teclado.nextInt();
-//
-//			aleatorio = (int) (Math.random() * 6);
-//			
-//			if(numeroDigitado != aleatorio) {
-//				System.out.printf("Você errou! O número correto era %s%n", aleatorio);
-//			}
-//
-//		} while (numeroDigitado != aleatorio);
-//
-//		System.out.println("Você acertou!");
-//
-//		teclado.close();
-		
-//		Crie um programa em Java que, utilizando a classe Scanner, pergunte ao usuário sua 
-//		data de nascimento e calcule a sua idade. Se o usuário tiver 18 anos ou mais, 
-//		escreva na tela “você é maior de idade”, mas caso tenha menos de 18 anos, escreva 
-//		“você é menor de idade”.
-		
+		System.out.print("Qual a sua data de nascimento (dd/mm/aaaa)? ");
+
+		String dataDigitada = teclado.nextLine();
+
+		Date dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(dataDigitada);
+		Calendar anoNascimento = Calendar.getInstance(TimeZone.getTimeZone("America/Sao_Paulo"));
+		anoNascimento.setTime(dataNascimento);
+
+		Calendar hoje = Calendar.getInstance(TimeZone.getTimeZone("America/Sao_Paulo"));
+		int anoAtual = hoje.get(Calendar.YEAR);
+
+		int idade = anoAtual - anoNascimento.get(Calendar.YEAR);
+
+		if (idade >= 18) {
+			System.out.println("Você é maior de idade");
+		} else {
+			System.out.println("Você é menor de idade");
+		}
+
+		teclado.close();
 
 	}
 }
